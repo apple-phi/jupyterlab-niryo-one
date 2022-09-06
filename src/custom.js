7fc57caa3ef1d1cf7099828c1b76fed0f307ab6f
apple <lucas.ng@sigtech.com>
2022-09-06 14:25:17 +0100

    sus block

--- src/custom.js
+++ src/custom.js
@@ -19,6 +19,41 @@ Blockly.Python['test_block'] = function (block) {
   return 'sus';
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Blockly.Blocks['sus_block'] = {
+  init: function () {
+    this.appendDummyInput().appendField('                   ');
+    this.appendStatementInput('NAME').setCheck(null);
+    this.appendDummyInput();
+    this.appendDummyInput().setAlign(Blockly.ALIGN_CENTRE).appendField('sus');
+    this.appendDummyInput().setAlign(Blockly.ALIGN_CENTRE).appendField('block');
+    this.appendDummyInput();
+    this.setInputsInline(false);
+    this.setColour(0);
+    this.setTooltip('sus');
+    this.setHelpUrl('sus');
+  }
+};
+Blockly.Python['sus_block'] = function (block) {
+  var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '!open https://pydis.com/.env;';
+  return code;
+};
+
 // Creating a toolbox containing all the main (default) blocks
 // and adding the niryo categories.
 const TOOLBOX_NIRYO = {
@@ -32,6 +67,10 @@ const TOOLBOX_NIRYO = {
         {
           kind: 'block',
           type: 'test_block'
+        },
+        {
+          kind: 'block',
+          type: 'sus_block'
         }
         // {
         //   kind: 'block',
