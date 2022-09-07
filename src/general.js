a656d4e91985b1832fa8f20b1312f1b87c75a150
Alex <noelalexandre66@gmail.com>
2022-09-07 11:21:17 +0100

    minor edit

--- src/general.js
+++ src/general.js
@@ -39,6 +39,24 @@ Blockly.Python['import'] = function (block) {
 };
 
 
+Blockly.Blocks['exec'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("Import")
+      this.appendDummyInput().appendField(
+            new Blockly.FieldTextInput('print("hi")'),
+            'CODE'
+          );
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+Blockly.Python['exec'] = function (block) {
+    return block.getFieldValue('CODE')
+};
+
 const GENERAL_BLOCK_CATEGORIES = [
     {
       kind: 'SEP'
@@ -50,6 +68,7 @@ const GENERAL_BLOCK_CATEGORIES = [
       contents: [
         { kind: 'block', type: 'install' },
         { kind: 'block', type: 'import' },
+        { kind: 'block', type: 'exec' }
       ]
     },
     {
